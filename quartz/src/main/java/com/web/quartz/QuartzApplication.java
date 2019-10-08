ca22182bd508ddfe0aaf89ad0e6d82408b366b02
jin_a <jin_a@DESKTOP-O1DFB21>
2019-10-09 00:12:46 +0900

    feat: 테스트용 SimpleScheduler 생성

--- quartz/src/main/java/com/web/quartz/QuartzApplication.java
+++ quartz/src/main/java/com/web/quartz/QuartzApplication.java
@@ -1,11 +1,17 @@
 package com.web.quartz;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.web.quartz.scheduler.SimpleScheduler;
 
 @SpringBootApplication
 public class QuartzApplication {
 
+	@Autowired
+	private SimpleScheduler sheduler;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzApplication.class, args);
 	}
